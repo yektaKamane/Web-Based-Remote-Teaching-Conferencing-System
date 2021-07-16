2fde56342ab1a581279b1805273b9fa586d683a3
yektakm <44814811+yektakm@users.noreply.github.com>
2021-07-16 19:24:37 +0430

    Add files via upload

--- chat.js
+++ chat.js
@@ -31,19 +31,25 @@ groupChat = {
     },
   
     send : function(){
-      var msg = document.getElementById("msg");
+      //var msg = document.getElementById("msg");
       if (msg.value.length > 0) {
         var newmsg = { user: this.username , text: msg.value };
-      this.mesgs.push(newmsg);
-      
-      msg.value = "";
-      this.chat_box.value = "";
-      var node = document.createElement("p");
-      var textnode = document.createTextNode(`${newmsg.user}: ${newmsg.text}`);
-      node.appendChild(textnode);
-      this.chat_box.appendChild(node);
+        //this.mesgs.push(newmsg); // get a newmsg and push it
+        groupChat.pushMssg(newmsg);
       }
-      
+
     },
+
+    pushMssg : function(newMssg){
+        this.mesgs.push(newMssg);
+        var msg = document.getElementById("msg");
+        msg.value = "";
+        this.chat_box.value = "";
+        var node = document.createElement("p");
+        var textnode = document.createTextNode(`${newMssg.user}: ${newMssg.text}`);
+        node.appendChild(textnode);
+        this.chat_box.appendChild(node);
+        
+    }
   
   }
