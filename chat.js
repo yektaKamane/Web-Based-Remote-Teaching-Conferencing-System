f7915f5540a9817a5475c6809defdaee6546309b
yektakm <44814811+yektakm@users.noreply.github.com>
2021-07-27 20:54:35 +0430

    Setting grades on messages

--- chat.js
+++ chat.js
@@ -1,18 +1,20 @@
 
+var msgID = 1;
 groupChat = {
-
-    mesgs : [
-      { user: "reza", text: "asfjhdjkfasaasfasfkadskfhadshfjkasl", inchat: false },
-      { user: "ali", text: "hello", inchat: false },
-      { user: "reza", text: "asffaf", inchat: false },
-      { user: "mmd", text: "asfjhdjkfkadskfhadshfjkasl", inchat: false },
-      { user: "reza", text: "asfjhdjkfkadskfhadshfjkasl", inchat: false },
-    ],
+    
+    mesgs :[
+      { user: "reza", text: "avali", score: null, isTrue: null, ID: msgID++ },
+      { user: "ali", text: "dovomi", score: null, isTrue: null, ID: msgID++ },
+      { user: "reza", text: "sevomi", score: null, isTrue: null, ID: msgID++ },
+      { user: "mmd", text: "char", score: null, isTrue: null, ID: msgID++ },
+      { user: "reza", text: "panj", score: null, isTrue: null, ID: msgID++ },
+  ],
   
     chat_box : "",
     username : "",
 
     init : function(){
+      this.msgID = 1;
         this.chat_box = document.getElementById("chatbox");
         this.username = attendees.user_name;
         for (var i in this.mesgs) {
@@ -35,7 +37,7 @@ groupChat = {
       groupChat.CreateReport();
     },
 
-    CreateReport : function (ReportName = ''){
+    CreateReport : function (ReportName = ''){     // Export to excel
       var downloadLink;
       var dataType = 'application/vnd.ms-excel';
       ReportName = ReportName?ReportName+'.xls':''+'Chatlist.xls';//modify excle sheet name here 
@@ -49,10 +51,12 @@ groupChat = {
       }else{
           downloadLink.href = 'data:' + dataType + ', ' ;
           downloadLink.href += `<table><tbody>
-          <tr><th>Name</th><th>Message</th></tr>`;
+          <tr><th>Name</th><th>Message</th><th>Score</th><th>Correctness</th></tr>`;
           for (i=0; i<this.mesgs.length; i++){
             downloadLink.href += '<tr><td>' + this.mesgs[i].user + '</td>';
-            downloadLink.href += '<td>' + this.mesgs[i].text + '</td></tr>';
+            downloadLink.href += '<td>' + this.mesgs[i].text + '</td>';
+            downloadLink.href += '<td>' + this.mesgs[i].score + '</td>';
+            downloadLink.href += '<td>' + this.mesgs[i].isTrue + '</td></tr>';
           }
           downloadLink.href += `</tbody><table>`;
           downloadLink.download = ReportName;
@@ -63,7 +67,7 @@ groupChat = {
     send : function(){
       var msg = document.getElementById("msg");
       if (msg.value.length > 0) {
-        var newmsg = { user: this.username , text: msg.value };
+        var newmsg = { user: this.username , text: msg.value, score: null, isTrue: null, ID: msgID++ };
         //this.mesgs.push(newmsg); // get a newmsg and push it
         groupChat.pushMssg(newmsg);
       }
@@ -75,11 +79,36 @@ groupChat = {
         var msg = document.getElementById("msg");
         msg.value = "";
         this.chat_box.value = "";
-        var node = document.createElement("p");
-        var textnode = document.createTextNode(`${newMssg.user}: ${newMssg.text}`);
-        node.appendChild(textnode);
-        this.chat_box.appendChild(node);
+        const html = `<div class="message">
+        <p class="msg-text">${newMssg.user} : ${newMssg.text}</p>
+       <form action="" name='message'>
+        <input type="text" name='score' id="score-${newMssg.ID}" />
+        <input type="checkbox" class="IsTrue" name='accept' id="isTrue-${newMssg.ID}">
+       </form>
+        </div>`
+
+      this.chat_box.insertAdjacentHTML('beforeend', html)
+      document.getElementById(`isTrue-${newMssg.ID}`).addEventListener('change', this.checkTrue)
+      document.getElementById(`score-${newMssg.ID}`).addEventListener('keyup', this.giveScore)
+
         
-    }
-  
+    },
+
+    checkTrue : function() { //         ********          in bayad ezafe she
+      let id = Number(this.id.slice(this.id.indexOf('-') + 1));
+      let hoof = groupChat.mesgs.find(m => m.ID == id);
+      if (this.checked) {
+          hoof.isTrue = true;
+      } else {
+          hoof.isTrue = null;
+      };
+  },
+
+   giveScore : function() { //         ********          in  bayad ezafe she
+    let id = Number(this.id.slice(this.id.indexOf('-') + 1));
+    let hoof = groupChat.mesgs.find(m => m.ID == id);
+    hoof.score = Number(this.value);
   }
+
+  
+}
